de9b55805b1966e4915ebd8e185b01e8832fb107
Nshan Petrosyan <nshanp@me.com>
2019-07-05 15:22:57 -0700

    assignment-006-todo-DOM-pt2 complete

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-function onReady() {
-  const toDos = [];
+function onReady(){
+  let toDos = [];
+  let id = 0;
   const addToDoForm = document.getElementById('addToDoForm');
 
-  function createNewToDo() {
-    const newToDoText = document.getElementById('newToDoText');
-    if (!newToDoText.value) { return; }
-
-    toDos.push({
-      title: newToDoText.value,
-      complete: false
-    });
-    newToDoText.value = '';
-
-    renderTheUI();
-  }
-
+  //RENDER THE UI
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo) {
@@ -25,22 +13,53 @@ function onReady() {
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = "Delete";
+
+      deleteBtn.addEventListener('click', event => {
+        toDos = toDos.filter(function(item){
+          return item.id !== toDo.id;
+        })
+
+        renderTheUI();
+      });
+
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteBtn);
+    })
+
+  }
+
+  //CREATE NEW TODO
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value){ return; }
+    toDos.push({
+      title: newToDoText.value,
+      complete: false,
+      id: id
     });
 
+    id++;
+
+    console.log(toDos);
+
+    newToDoText.value = '';
+    renderTheUI();
   }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
+    console.log(toDos);
   });
 
   renderTheUI();
 }
 
-window.onload = function () {
+window.onload = function() {
   onReady();
 };
